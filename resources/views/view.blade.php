e228651c8534486120703222ebdd88fc8f2928ad
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2023-09-28 00:31:55 +0800

    Fix view page current url route

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.main')
 @section('content')
     <p>{{ $site->full_path }}</p>
-    <a href="{{ route('redirect', ['url' => $site->new_path]) }}"
-        target="_blank">http://localhost:8000/{{ $site->new_path }}</a>
+    <a href="{{ route('redirect', ['url' => $site->new_path]) }}" target="_blank">
+        {{ Request::root() }}/{{ $site->new_path }}
+    </a>
 @endsection
