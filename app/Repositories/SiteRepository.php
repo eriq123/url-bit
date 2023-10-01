5fc00b3b4059ebe23fb2b0beb431af9049a0bb65
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2023-10-01 19:33:38 +0800

    Add  dynamic unique path length

--- app/Repositories/SiteRepository.php
+++ app/Repositories/SiteRepository.php
@@ -8,14 +8,37 @@ use Illuminate\Support\Str;
 
 class SiteRepository implements SiteInterface
 {
+    private $uniquePathLength = 4;
+
     public function getSiteByNewPath($newPath)
     {
         return Site::where('new_path', $newPath)->first();
     }
 
-    private function getUniquePath($length = 4)
+    private function getMaxUniquePathCombinations($length)
+    {
+        $lowercaseLetters = 26;
+        $uppercaseLetters = 26;
+
+        return pow(($lowercaseLetters + $uppercaseLetters), $length);
+    }
+
+    private function getUniquePathLength($length, $arrNewPaths)
+    {
+        if (!isset($length)) $length = $this->uniquePathLength;
+
+        if (count($arrNewPaths) >= $this->getMaxUniquePathCombinations($length)) {
+            return $this->getUniquePathLength(++$length, $arrNewPaths);
+        }
+
+        return $length;
+    }
+
+    private function getUniquePath($length)
     {
         $arrSiteNewPaths = Site::pluck('new_path')->toArray();
+        $length = $this->getUniquePathLength($length, $arrSiteNewPaths);
+
         do {
             $uniquePath = Str::random($length);
         } while (in_array($uniquePath, $arrSiteNewPaths));
